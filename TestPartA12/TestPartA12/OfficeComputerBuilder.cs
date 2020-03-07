e99e5c0be92513933c99d813154229a36897f277
elyzinger <45950112+elyzinger@users.noreply.github.com>
2020-03-07 18:44:01 +0200

    Add files via upload

---
+++ TestPartA12/TestPartA12/OfficeComputerBuilder.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestPartA12
+{
+    class OfficeComputerBuilder : ComputerBuilder
+    {
+        public override void AddingGraphicCard()
+        {
+            computer.AddingGraphicCard("normal");
+        }
+
+        public override void AddingMemoryCard()
+        {
+            computer.AddingMemoryCard("normal");
+        }
+
+        public override void AddingMotherboard()
+        {
+            computer.AddingMotherboard("normal");
+        }
+
+        public override void AddingProcessor()
+        {
+            computer.AddingProcessor("normal");
+        }
+
+        public override void BuildCase()
+        {
+            computer.BuildCase("normal");
+        }
+
+        public override void RunTest()
+        {
+            computer.RunTest();
+        }
+    }
+}
---
+++ TestPartA12/TestPartA12/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestPartA12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<ComputerBuilder> comBuilder = new List<ComputerBuilder>();
+            comBuilder.Add(new GamingComputerBuilder());
+            comBuilder.Add(new HouseComputerBuilder());
+            comBuilder.Add(new OfficeComputerBuilder());
+
+            Computer com = CreateCom(comBuilder[0]);
+            Console.WriteLine(comBuilder[0]);
+            Console.WriteLine(comBuilder[1].ToString());
+        }
+
+        static Computer CreateCom(ComputerBuilder builder)
+        {
+            builder.BuildComputer();
+            return builder.GetComputer();
+
+        }
+
+        
+    }
+}
